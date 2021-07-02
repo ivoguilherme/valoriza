6a5e8dd9a8fa471fdd5ef9b46483cc1f28bcb9a1
Ivo Guilherme <ivo.guiilherme@gmail.com>
2021-07-02 17:52:39 -0300

    Create user

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,31 @@
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories";
+
+interface IUserRequest {
+  name: string;
+  email: string;
+  admin?: boolean;
+}
+
+class CreateUserService {
+  async execute({ name, email, admin }: IUserRequest) {
+    if (!email) throw new Error('Incorrect e-mail');
+
+    const usersRepositories = getCustomRepository(UsersRepositories);
+    const userAlreadyExists = await usersRepositories.findOne({email});
+
+    if (userAlreadyExists) throw new Error('User already exists');
+
+    const user = usersRepositories.create({
+      name,
+      email,
+      admin
+    });
+
+    await usersRepositories.save(user);
+
+    return user;
+  }
+}
+
+export { CreateUserService }
